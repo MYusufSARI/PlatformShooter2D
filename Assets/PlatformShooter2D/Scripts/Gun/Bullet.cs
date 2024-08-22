d2234f84c689c8d70100d1c9d0030190c9cdfa3b
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-08-22 03:56:23 +0300

    Added MachineGun

--- Assets/PlatformShooter2D/Scripts/Gun/Bullet.cs
+++ Assets/PlatformShooter2D/Scripts/Gun/Bullet.cs
@@ -4,25 +4,27 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [Header(" Settings ")]
     [SerializeField] private float _moveSpeed = 10f;
     [SerializeField] private int _damageAmount = 1;
 
+    [Header(" Elements ")]
     private Vector2 _fireDirection;
-
     private Rigidbody2D _rigidBody;
+
+
 
     private void Awake()
     {
         _rigidBody = GetComponent<Rigidbody2D>();
     }
 
-    private void Start() {
-        if (PlayerController.Instance.IsFacingRight()) {
-            _fireDirection = Vector2.right;
-        } else {
-            _fireDirection = Vector2.left;
-        }
+
+    public void Initialize(Vector2 bulletSpawnPos, Vector2 mousePos)
+    {
+        _fireDirection = (mousePos - bulletSpawnPos).normalized;
     }
+
 
     private void FixedUpdate()
     {
--- Assets/PlatformShooter2D/Scripts/Gun/Gun.cs
+++ Assets/PlatformShooter2D/Scripts/Gun/Gun.cs
@@ -6,23 +6,56 @@ public class Gun : MonoBehaviour
 {
     public Transform BulletSpawnPoint => _bulletSpawnPoint;
 
+    [Header(" Elements ")]
     [SerializeField] private Transform _bulletSpawnPoint;
     [SerializeField] private Bullet _bulletPrefab;
-    
+    private Camera mainCamera;
+
+    [Header(" Settings ")]
+    [SerializeField] private float _gunFireCD = 0.5f;
+    private Vector2 _mousePos;
+    private float _lastFireTime = 0f;
+
+
+
+    private void Start()
+    {
+        mainCamera = Camera.main;   
+    }
+
+
     private void Update()
     {
         Shoot();
+
+        RotateCamera();
     }
+
 
     private void Shoot()
     {
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButton(0) && Time.time >= _lastFireTime) {
             ShootProjectile();
         }
     }
 
+
     private void ShootProjectile()
     {
+        _lastFireTime = Time.time + _gunFireCD;
+
         Bullet newBullet = Instantiate(_bulletPrefab, _bulletSpawnPoint.position, Quaternion.identity);
+
+        newBullet.Initialize(_bulletSpawnPoint.position, _mousePos);
+    }
+
+
+    private void RotateCamera()
+    {
+        _mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = PlayerController.Instance.transform.InverseTransformPoint(_mousePos);
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.localRotation = Quaternion.Euler(0, 0, angle);
     }
 }
