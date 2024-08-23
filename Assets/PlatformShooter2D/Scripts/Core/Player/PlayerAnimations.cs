5145bb4bb19aa10ab1761507b076478796038249
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-08-23 08:27:19 +0300

    Added details

--- Assets/PlatformShooter2D/Scripts/Core/Player/PlayerAnimations.cs
+++ Assets/PlatformShooter2D/Scripts/Core/Player/PlayerAnimations.cs
@@ -7,10 +7,12 @@ public class PlayerAnimations : MonoBehaviour
     [Header(" Elements ")]
     [SerializeField] private ParticleSystem _moveDustVFX;
     [SerializeField] private Transform _characterSpriteTransform;
+    [SerializeField] private Transform _cowboyHatTransform;
 
     [Header(" Settings ")]
     [SerializeField] private float _tiltAngle = 20f;
     [SerializeField] private float _tiltSpeed = 5f;
+    [SerializeField] private float _cowboyHatModifier = 2f;
 
 
 
@@ -61,11 +63,20 @@ public class PlayerAnimations : MonoBehaviour
             targetAngle = 0f;
         }
 
+        //Player Sprite
         Quaternion currentCharacterRotation = _characterSpriteTransform.rotation;
         Quaternion targetCharacterRotation =
             Quaternion.Euler(currentCharacterRotation.eulerAngles.x, currentCharacterRotation.eulerAngles.y, targetAngle);
 
         _characterSpriteTransform.rotation =
             Quaternion.Lerp(currentCharacterRotation, targetCharacterRotation, _tiltSpeed * Time.deltaTime); ;
+
+        //Cowboy Hat Sprite
+        Quaternion currentHatRotation = _cowboyHatTransform.rotation;
+        Quaternion targetHatRotation =
+            Quaternion.Euler(currentHatRotation.eulerAngles.x, currentHatRotation.eulerAngles.y, -targetAngle / _cowboyHatModifier);
+
+        _cowboyHatTransform.rotation =
+            Quaternion.Lerp(currentHatRotation, targetHatRotation, _tiltSpeed * _cowboyHatModifier * Time.deltaTime); ;
     }
 }
