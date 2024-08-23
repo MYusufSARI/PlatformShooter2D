4d06361698250b607e7e5186215da075a16e4287
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-08-23 07:09:25 +0300

    Added bullet VFX

--- Assets/PlatformShooter2D/Scripts/Core/Gun/Bullet.cs
+++ Assets/PlatformShooter2D/Scripts/Core/Gun/Bullet.cs
@@ -9,9 +9,10 @@ public class Bullet : MonoBehaviour
     [SerializeField] private float _moveSpeed = 10f;
     [SerializeField] private int _damageAmount = 1;
     [SerializeField] private float _knockbackThrust = 20f;
-    [SerializeField]
 
     [Header(" Elements ")]
+    [SerializeField] private GameObject _bulletVFX;
+
     private Vector2 _fireDirection;
     private Rigidbody2D _rigidBody;
 
@@ -44,6 +45,8 @@ public class Bullet : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Instantiate(_bulletVFX, transform.position, Quaternion.identity);
+
         Health health = other.gameObject.GetComponent<Health>();
         health?.TakeDamage(_damageAmount);
 
